31b9c5c6b27e8153492880b6069e221f0d1698ff
Tomas Doran <t0m@the.internet.never.works>
2024-09-09 16:27:12 +0100

    Make the data pass the test

--- app/data/CV/__tests__/data.ts
+++ app/data/CV/__tests__/data.ts
@@ -1,5 +1,14 @@
 import { loadData } from '../Data'
+import { CVSchema } from '../Schema'
 
 test('can loadData', () => {
-    expect(loadData()).toBeDefined();
-  });
+  expect(loadData()).toBeDefined()
+})
+
+test('parse', () => {
+    const data = loadData()
+    const result = CVSchema.safeParse(data)
+    expect(result.error).toEqual(undefined)
+    expect(result.success).toEqual(true)
+    expect(result.data).toBeDefined()
+})
