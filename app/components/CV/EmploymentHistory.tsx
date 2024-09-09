0fda9117da04ae133ddf04082ba481f221d6b93f
Tomas Doran <t0m@the.internet.never.works>
2024-09-09 20:42:02 +0100

    Displaying everything properly

--- app/components/CV/EmploymentHistory.tsx
+++ app/components/CV/EmploymentHistory.tsx
@@ -1,13 +1,44 @@
+import { MarkdownString } from '../Markdown'
+
 import type { Employment as EmploymentData } from '~/data/CV'
 
 export type EmploymentHistoryProps = {
   employmentHistoryData: EmploymentData[]
 }
 
+function EmploymentDates ({ employment }: { employment: EmploymentData }) {
+  const numPositions = employment.positions.length
+  const endDate = employment.positions[0].endDate
+  const startDate = employment.positions[numPositions-1].startDate
+  if (numPositions == 1) {
+    return (
+      <div>{startDate} - {endDate}: {employment.positions[0].title}</div>
+    )
+  }
+  return (
+    <div>
+      <div>{startDate} - {endDate}</div>
+      <ul>
+        {employment.positions.map(position => (
+          <li key={position.title}>{position.startDate} - {position.endDate}: {position.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 function EmploymentItem ({ employment }: { employment: EmploymentData }) {
   return (
     <div>
-      <h3>{employment.companyName}</h3>
+      <h3><a href={employment.url}>{employment.companyName}</a></h3>
+      <EmploymentDates employment={employment} />
+      <h4>Accomplishments:</h4>
+      <ul className="accomplishments">
+        {employment.accomplishments.map((accomplishment, idx) => (
+          <li key={idx}>
+            <MarkdownString markdown={accomplishment.description} />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
