e4adf8d298bd43430c6b8ed819037dceed63e3bb
Tomas Doran <t0m@the.internet.never.works>
2024-09-09 20:47:50 +0100

    Fix eslint

--- handler.js
+++ handler.js
@@ -6,6 +6,7 @@ installGlobals()
 
 export const handler = createRequestHandler({
   build, 
+  // eslint-disable-next-line no-undef
   mode: process.env.NODE_ENV,
   awsProxy: AWSProxy.APIGatewayV2,
 });
