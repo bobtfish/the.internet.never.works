27aa0cd9e6fe1f4259b4a606506b33af7253115f
Tomas Doran <t0m@the.internet.never.works>
2024-09-09 21:03:44 +0100

    Hopefully fix server side by getting vite to import the YAML, lol

--- app/components/Markdown/MarkdownParagraph.tsx
+++ app/components/Markdown/MarkdownParagraph.tsx
@@ -3,8 +3,8 @@ import { MarkdownString } from './MarkdownString'
 
 export function MarkdownParagraph({ markdown }: { markdown: string | React.ReactNode[] }) {
     const paras = splitParagraphs(markdown)
-    return <div>{paras.map((para) => {
+    return <div>{paras.map((para, idx) => {
         // eslint-disable-next-line react/jsx-key
-        return <p><MarkdownString markdown={para} /></p>
+        return <p key={idx}><MarkdownString markdown={para} /></p>
     }, markdown)}</div>
 }
--- app/data/CV/Data.ts
+++ app/data/CV/Data.ts
@@ -1,9 +1,6 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import { parse as yamlParse } from 'yaml';
+import YamlContent from './data.yaml'
 import { CV } from './Schema'
 
 export function loadData(): CV {
-    return yamlParse(fs.readFileSync(path.join(path.dirname(fileURLToPath(import.meta.url)), 'data.yaml'), 'utf8'))
+    return YamlContent as CV;
 }
