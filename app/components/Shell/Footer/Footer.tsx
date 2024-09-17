c51d14325aa81f51a0c0a9bfcb589d5fb574eb78
Tomas Doran <t0m@the.internet.never.works>
2024-09-18 00:08:31 +0100

    Make the footer responsive too

--- app/components/Shell/Footer/Footer.tsx
+++ app/components/Shell/Footer/Footer.tsx
@@ -1,19 +1,23 @@
-import { Text } from '@mantine/core'
+import { Text, Box } from '@mantine/core'
 import { AppleIcon, RemixIcon, TypeScriptIcon, MantineIcon, VSCodeIcon, LambdaIcon, CDKIcon, TerraformIcon } from '~/components'
 import classes from './Footer.module.css'
 
 export function Footer () {
   return (
     <Text color='dimmed' ta='center' size='sm' mx='auto' mt='xs'>
-      Built with ❤️ on <AppleIcon className={classes.footerIcon} /> 
+      Built with ❤️&nbsp;
+      <Box component='span' className={classes.hideXSmall}>on <AppleIcon className={classes.footerIcon} /> 
       with <VSCodeIcon className={classes.footerIcon} href="https://code.visualstudio.com/" />
       in <TypeScriptIcon className={classes.footerIcon} href="https://www.typescriptlang.org/" />
+      </Box>
       using <RemixIcon className={classes.footerIcon} href="https://remix.run/" />
       and <MantineIcon size={18} className={classes.footerIcon} style={{marginRight: 0}} href="https://mantine.dev/" />.
       Deployed on <LambdaIcon className={classes.footerIcon} href="https://aws.amazon.com/pm/lambda/" />
-      with <CDKIcon className={classes.footerIcon} href="https://aws.amazon.com/cdk/" />
-      and <TerraformIcon size={20} className={classes.footerIcon} href="https://www.terraform.io/" />
-      &nbsp;&nbsp;&nbsp;Content &copy; Tomas Doran 2024
+      with <Box component='span' className={classes.hideXSmall}><CDKIcon className={classes.footerIcon} href="https://aws.amazon.com/cdk/" />
+      and</Box> <TerraformIcon size={20} className={classes.footerIcon} href="https://www.terraform.io/" />
+      <Box component='span' className={classes.hideSmall}>
+      &nbsp;&nbsp;&nbsp;&copy; Tomas Doran 2024
+      </Box>
     </Text>
   )
 }
